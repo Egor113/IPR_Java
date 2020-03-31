600a5f6ba531d1229704e236a2b903f5e11a236d
Egor Ogdanets <ogdanets2010@yandex.ru>
2020-03-31 19:07:32 +0300

    Optimized code

--- src/main/java/task1/LogSplitter.java
+++ src/main/java/task1/LogSplitter.java
@@ -4,8 +4,9 @@ import java.io.*;
 
 public class LogSplitter {
 
-    public static int getFileLength(String fileName){
-        int fileSize = 0;
+    static int fileSize = 0;
+
+    public static void getFileLength(String fileName){
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String str;
             while ((str = reader.readLine()) != null ) {
@@ -14,46 +15,24 @@ public class LogSplitter {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return fileSize;
     }
 
-    public static void splitFile(String fileName, int fileSize) {
+    public static void splitFile(String fileName) {
         int newFileSize = fileSize / 10;
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String fileNameTemplate = "log";
             if (fileSize % 10 == 0) {
                 for (int i = 0; i < 10; i++) {
                     int num = i + 1;
-                    String currentFileName = fileNameTemplate + " " + num;
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFileName))){
-                        for (int j = 0; j < newFileSize; j++) {
-                            writer.write(reader.readLine() + "\n");
-                        }
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
+                    writeIntoFile(fileNameTemplate + "_" + num + ".log", newFileSize, reader,true);
                 }
             }
             else {
                 for (int i = 0; i < 9; i++) {
                     int num = i + 1;
-                    String currentFileName = fileNameTemplate + " " + num;
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFileName))){
-                        for (int j = 0; j < newFileSize; j++) {
-                            writer.write(reader.readLine() + "\n");
-                        }
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
+                    writeIntoFile(fileNameTemplate + "_" + num + ".log", newFileSize, reader,true);
                 }
-                String currentFileName = fileNameTemplate + " 10";
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFileName))){
-                    for (int j = 0; j < newFileSize + fileSize % 10; j++) {
-                        writer.write(reader.readLine() + "\n");
-                    }
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+                writeIntoFile(fileNameTemplate + "_10.log", newFileSize, reader,false);
             }
         }
         catch (IOException e) {
@@ -61,8 +40,30 @@ public class LogSplitter {
         }
     }
 
+    public static void writeIntoFile(String currentFileName, int newFileSize, BufferedReader reader, boolean isDividedBy10){
+        if (isDividedBy10){
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFileName))){
+                for (int j = 0; j < newFileSize; j++) {
+                    writer.write(reader.readLine() + "\n");
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        else {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFileName))){
+                for (int j = 0; j < newFileSize + fileSize % 10; j++) {
+                    writer.write(reader.readLine() + "\n");
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        splitFile("main.log.2014-11-17",(getFileLength("main.log.2014-11-17")));
+        getFileLength("main.log");
+        splitFile("main.log");
     }
 
 }
